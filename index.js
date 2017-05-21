aaf35c40a9fc86002c98d920888bf3ab6f266ed8
Weiming Zhai <wmzhai@gmail.com>
2017-05-22 00:19:15 +0800

    mainWindow closed

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let addWindow
 app.on('ready', () => {
   mainWindow = new BrowserWindow()
   mainWindow.loadURL(`file://${__dirname}/main.html`)
+  mainWindow.on('closed', () => app.quit())
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate)
   Menu.setApplicationMenu(mainMenu)
@@ -19,6 +20,7 @@ function createAddWindow() {
     height: 200,
     title: 'Add New Todo'
   })
+  addWindow.loadURL(`file://${__dirname}/add.html`)
 }
 
 const menuTemplate = [
